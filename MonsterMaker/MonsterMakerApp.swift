ea531ac92a9c6340451450c137b65a18a7bb2098
lmcmz <lmcmze@gmail.com>
2022-11-01 11:23:52 +1100

    Initial Commit

---
+++ MonsterMaker/MonsterMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonsterMakerApp.swift
+//  MonsterMaker
+//
+//  Created by Hao Fu on 1/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct MonsterMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
