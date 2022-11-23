ce7b552e05292d0e6c56cedabda31ef06949872e
Jonathan Bluks <jonathan.bluks@dapperlabs.com>
2022-11-23 15:49:01 -0800

    Add Wallet Connection

--- server/pages/_app.tsx
+++ server/pages/_app.tsx
@@ -1,9 +1,13 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Web3ContextProvider } from 'contexts/Web3';
 
 const App = ({ Component, pageProps }: AppProps) => {
-  // TODO: Initiate FCL here to maintain state between route changes
-  return <Component {...pageProps} />;
+  return (
+    <Web3ContextProvider network={process.env.NEXT_PUBLIC_FLOW_ENV}>
+      <Component {...pageProps} />
+    </Web3ContextProvider>
+  );
 };
 
 export default App;
