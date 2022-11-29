47cb19509b7c7e4869881621f3b0b97452887d45
Jonathan Bluks <jonathan.bluks@dapperlabs.com>
2022-11-29 14:03:33 -0800

    Update mint to use api

--- server/pages/create.tsx
+++ server/pages/create.tsx
@@ -1,7 +1,6 @@
 import Button from 'components/Button';
 import CreatePage from 'components/CreatePage';
 import MintButton from 'components/MintButton';
-import { useWeb3Context } from 'contexts/Web3';
 import ActionPanel from 'layout/ActionPanel';
 import Header from 'layout/Header';
 import NavPanel from 'layout/NavPanel';
@@ -9,7 +8,6 @@ import PageContainer from 'layout/PageContainer';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 import { ROUTES } from 'utils/constants';
-import mintMonsterScript from '../cadence/transactions/mintMonster';
 import {
   backgroundRange,
   headRange,
@@ -17,26 +15,35 @@ import {
   torsoRange,
 } from 'utils/mapAssets';
 import usePartSelector from 'hooks/usePartSelector';
+import { userAgent } from 'next/server';
+import { useWeb3Context } from 'contexts/Web3';
 
 const Create = () => {
   const router = useRouter();
+  const { user } = useWeb3Context();
   const [isMintInProgress, setIsMintInProgress] = useState<boolean>(false);
-  const { user, executeTransaction } = useWeb3Context();
   const [bgIndex] = usePartSelector(backgroundRange);
   const [headIndex] = usePartSelector(headRange);
   const [torsoIndex] = usePartSelector(torsoRange);
   const [legsIndex] = usePartSelector(legsRange);
 
   const mintMonster = async () => {
-    const args = (arg: any, t: any) => [
-      arg(user.addr, t.Address),
-      arg(bgIndex, t.Int),
-      arg(headIndex, t.Int),
-      arg(torsoIndex, t.Int),
-      arg(legsIndex, t.Int),
-    ];
-
-    await executeTransaction(mintMonsterScript, args, {});
+    const response = await fetch('/api/mint', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        address: user.addr,
+        components: {
+          background: bgIndex,
+          head: headIndex,
+          torso: torsoIndex,
+          legs: legsIndex,
+        },
+      }),
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
   };
 
   const handleClickMint = async () => {
