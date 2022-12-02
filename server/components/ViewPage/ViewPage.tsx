ae0c5cf6e5445511cfa298f9afa106dc92aa507f
Luke Fiji <luke.fiji@gmail.com>
2022-12-01 16:07:09 -0800

    Sort monsters by id

--- server/components/ViewPage/ViewPage.tsx
+++ server/components/ViewPage/ViewPage.tsx
@@ -1,7 +1,7 @@
 import NFTView from 'components/NFTView/NFTView';
 import useEmblaCarousel from 'embla-carousel-react';
-import { useEffect } from 'react';
-import { GetMonstersResponse } from 'utils/types';
+import { useEffect, useMemo } from 'react';
+import { GetMonstersResponse, Monster } from 'utils/types';
 import styles from './ViewPage.module.css';
 
 interface Props {
@@ -18,11 +18,19 @@ const ViewPage = ({ monsters }: Props) => {
     }
   }, [emblaApi, monsters]);
 
+  const descendingOrderMonsters = useMemo(
+    () =>
+      [...monsters].sort(
+        (a, b) => parseInt(b.itemID, 10) - parseInt(a.itemID, 10),
+      ),
+    [monsters],
+  );
+
   return (
     <main className={styles.main}>
       <div className={styles.embla} ref={emblaRef}>
         <div className={styles.emblaContainer}>
-          {monsters.map(({ resourceID, component }) => {
+          {descendingOrderMonsters.map(({ resourceID, component }) => {
             return (
               <div key={resourceID} className={styles.emblaSlide}>
                 <NFTView
