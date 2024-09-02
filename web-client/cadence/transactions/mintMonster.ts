251aa62fd6f62cf537ad430a4c842adc041633f6
Caos <caosbad@gmail.com>
2024-09-02 12:11:41 +0800

    update file

--- web-client/cadence/transactions/mintMonster.ts
+++ web-client/cadence/transactions/mintMonster.ts
@@ -27,45 +27,45 @@ transaction(
     let mintingIDBefore: UInt64
 
     // The Vault resource that holds the tokens that are being transfered
-    let sentVault: @FungibleToken.Vault
+    let sentVault: @{FungibleToken.Vault}
 
     // The Minter will receive the FungibleToken
     let minterReceiver: &{FungibleToken.Receiver}
 
-    prepare(minter: AuthAccount, recipient: AuthAccount) {
+    prepare(minter: auth(Storage, BorrowValue) &Account, recipient: auth(Storage, Capabilities) &Account) {
         self.mintingIDBefore = MonsterMaker.totalSupply
 
         // if the account doesn't already have a collection
-        if recipient.borrow<&MonsterMaker.Collection>(from: MonsterMaker.CollectionStoragePath) == nil {
+        if recipient.storage.borrow<&MonsterMaker.Collection>(from: MonsterMaker.CollectionStoragePath) == nil {
 
             // create a new empty collection
-            let collection <- MonsterMaker.createEmptyCollection()
+            let collection <- MonsterMaker.createEmptyCollection(nftType: Type<@MonsterMaker.NFT>())
 
             // save it to the account
-            recipient.save(<-collection, to: MonsterMaker.CollectionStoragePath)
+            recipient.storage.save(<-collection, to: MonsterMaker.CollectionStoragePath)
 
-            // create a public capability for the collection
-            recipient.link<&{NonFungibleToken.CollectionPublic, MonsterMaker.MonsterMakerCollectionPublic, MetadataViews.ResolverCollection}>(MonsterMaker.CollectionPublicPath, target: MonsterMaker.CollectionStoragePath)
+            let collectionCap = recipient.capabilities.storage.issue<&{NonFungibleToken.Receiver, NonFungibleToken.CollectionPublic, MonsterMaker.MonsterMakerCollectionPublic}>(MonsterMaker.CollectionStoragePath)
+            recipient.capabilities.publish(collectionCap, at: MonsterMaker.CollectionPublicPath)
         }
 
 
         // Borrow a reference to the NFTMinter resource in storage
-        self.minter = minter.borrow<&MonsterMaker.NFTMinter>(from: MonsterMaker.MinterStoragePath)
+        self.minter = minter.storage.borrow<&MonsterMaker.NFTMinter>(from: MonsterMaker.MinterStoragePath)
             ?? panic("Could not borrow a reference to the NFT minter")
 
         // Borrow the recipient's public NFT collection reference
         self.recipientCollectionRef = recipient
-            .getCapability(MonsterMaker.CollectionPublicPath)
-            .borrow<&{NonFungibleToken.CollectionPublic}>()
+            .capabilities.get<&{NonFungibleToken.CollectionPublic}>(MonsterMaker.CollectionPublicPath)
+            .borrow()
             ?? panic("Could not get receiver reference to the NFT Collection")
 
         // Get a reference to the minter's Receiver
-        self.minterReceiver = minter.getCapability(/public/flowTokenReceiver)!
-            .borrow<&{FungibleToken.Receiver}>()
+        self.minterReceiver = minter.capabilities.get<&{FungibleToken.Receiver}>(/public/flowTokenReceiver)!
+            .borrow()
             ?? panic("Could not borrow receiver reference to the recipient's Vault")
 
         // Get a reference to the recipient's stored vault
-        let vaultRef = recipient.borrow<&FlowToken.Vault>(from: /storage/flowTokenVault)
+        let vaultRef = recipient.storage.borrow<auth(FungibleToken.Withdraw) &FlowToken.Vault>(from: /storage/flowTokenVault)
             ?? panic("Could not borrow reference to the owner's Vault!")
 
         // Withdraw tokens from the signer's stored vault
@@ -73,7 +73,7 @@ transaction(
     }
 
     execute {
-        let componentValue = MonsterMaker.MonsterComponent(background: background, head: head, torso: torso, leg: leg)
+        let componentValue = MonsterMaker.MonsterComponent(background: background, head: head, torso: torso, legs: leg)
 
         // TODO: Add royalty feature to MM using beneficiaries, cuts, and descriptions. At the moment, we don't provide royalties with KI, so this will be an empty list.
         let royalties: [MetadataViews.Royalty] = []
